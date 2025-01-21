991cde6985a230074184745a80890b9d69ed19b1
Adam Wright <32560955+adamdenverco@users.noreply.github.com>
2025-01-21 10:44:36 -0600

    continue learning

--- index.py
+++ index.py
@@ -1,2 +1,44 @@
 # test file
+# print("hello world")
 
+# modulo
+# first_order_remainder = 269 % 10
+# print(first_order_remainder)
+
+# first_order_coupon = "yes" if first_order_remainder == 0 else "no"
+# print(first_order_coupon)
+
+# second_order_remainder = 270 % 10
+# print(second_order_remainder)
+
+# second_order_coupon = "yes" if second_order_remainder == 0 else "no"
+# print(second_order_coupon)
+
+
+# total_price = 0
+
+# new_sneakers = 50.00
+
+# total_price += new_sneakers
+
+# nice_sweater = 39.00
+# fun_books = 20.00
+# # Update total_price here:
+
+# total_price += nice_sweater + fun_books
+
+# print("The total price is", total_price)
+
+# to_you = """
+# Stranger, if you passing meet me and desire to speak to me, why
+#   should you not speak to me?
+# And why should I not speak to you?
+# """
+
+# print(to_you)
+
+# my_age = 45
+# half_my_age = my_age / 2
+# greeting = "Hello there"
+# name = "George"
+# greeting_with_name = greeting + ", " + name
